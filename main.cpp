6175a7b44db66d98a97b04260dabd86f197a2c3a
GitGuru420 <raisulislam1892@gmail.com>
2025-01-29 14:41:05 +0600

    Display All Students

--- main.cpp
+++ main.cpp
@@ -35,6 +35,12 @@ class Student {
         int getAge() {
             return age;
         }
+
+        void displayStudent() {
+            cout << "ID No: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+        }
 };
 
 // add new student
@@ -50,11 +56,24 @@ void addNewStudent(vector<Student> &students) {
             return;
         }
     }
-    cout << "Enter Name: "; cin >> name;
+    cin.ignore();
+    cout << "Enter Full Name: "; getline(cin, name);
     cout << "Enter Age: "; cin >> age;
     
     students.push_back(Student(id, name, age));
     cout << "Student Add Successfully" << endl;
+}
+
+// display all students
+void displayAllStudents(vector<Student> &students) {
+    if(students.empty()) {
+        cout << "No Students Found" << endl;
+        return;
+    }
+    for(int i=0; i<students.size(); i++) {
+        students[i].displayStudent();
+        cout << endl;
+    }
 }
 
 
@@ -62,18 +81,25 @@ int main()
 {
     vector<Student> students;
 
-    cout << "\"Student Management System\"" << endl;
-    cout << "1. Add New Student" << endl;
-    cout << "2. Display All Students" << endl;
-    cout << "3. Search Student" << endl;
-    cout << "4. Update Student" << endl;
-    cout << "5. Delete Student" << endl;
-    cout << "6. Exit" << endl;
-    cout << "Enter Your Option: ";
-    int option; cin >> option;
+    char choice; 
+    do {
+        cout << "\"Student Management System\"" << endl;
+        cout << "1. Add New Student" << endl;
+        cout << "2. Display All Students" << endl;
+        cout << "3. Search Student" << endl;
+        cout << "4. Update Student" << endl;
+        cout << "5. Delete Student" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter Your Option: ";
+        int option; cin >> option;
 
-    switch(option) {
-        case 1: addNewStudent(students); break;
-    }
+        switch(option) {
+            case 1: addNewStudent(students); break;
+            case 2: displayAllStudents(students); break;
+            case 6: exit(1);
+        }
+        cout << "Do you want to continue ? Yes / No [Y / N]: "; 
+        cin >> choice;
+    } while(choice == 'Y' || choice == 'y');
     return 0;
 }
